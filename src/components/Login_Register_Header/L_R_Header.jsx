6957e504c317fef5d548a75e812cf272bdb392d9
Farzan Ali <farzan.16.fa@gmail.com>
2026-05-31 23:31:31 +0500

    add new header for login and register component

---
+++ src/components/Login_Register_Header/L_R_Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function L_R_Header() {
+  return (
+     <div className="bg-[#1B3A6B] p-2 w-full">
+        <div className="w-full max-w-6xl flex items-center mx-auto relative px-4 py-2">
+
+          <div>
+           <h1 className="font-bold text-3xl text-[#F7F7F5]">careerTrack.</h1>
+          </div>
+          </div>
+          </div>
+  )
+}
+
+export default L_R_Header
