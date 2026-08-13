0f53c863966a6a07ad3f792d263d9cfee4c1cf37
Farzan Ali <farzan.16.fa@gmail.com>
2026-08-13 15:59:22 +0500

    Fix routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
+import { Route, Routes } from "react-router-dom"
+import Layout from "./Layout"
+import Home from './components/Home/Home'
+import Login from './components/Login/Login'
+import Register from './components/Register/Register'
+import Application from './components/Application/Applications'
+import Account from "./components/Account/Account"
 function App() {
   return (
     <>
+     <Routes>
+      <Route path="/" element={<Layout/>}>
+      <Route path="" element={<Login/>}/>
+      <Route path="home" element={<Home/>}/>
+      <Route path="register" element={<Register/>}/>
+      <Route path="application" element={<Application/>}/>
+      <Route path="account" element={<Account/>}/>
+      </Route>
      
+     </Routes>
     </>
   )
 }
