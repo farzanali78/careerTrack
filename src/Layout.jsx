d887dce95f250608dcd56e4316812e3ce8dc7347
Farzan Ali <farzan.16.fa@gmail.com>
2026-08-14 19:06:15 +0500

    Add toast container

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -4,16 +4,29 @@ import { Outlet } from 'react-router-dom';
 import Footer from './components/Footer/Footer'
 import { useLocation } from 'react-router-dom';
 import L_R_Header from './components/Login_Register_Header/L_R_Header';
+import { ToastContainer, toast, Bounce } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function Layout() {
   const location = useLocation()
   const authroutes = ['/','/register']
   return (
-    <>
-    {authroutes.includes(location.pathname) ? <L_R_Header/> : <Header/>}
-    <Outlet/>
-     {!authroutes.includes(location.pathname) && <Footer/>}
-    </>
+    <><ToastContainer
+      position="top-right"
+      autoClose={5000}
+      hideProgressBar={false}
+      newestOnTop={false}
+      closeOnClick={false}
+      rtl={false}
+      pauseOnFocusLoss
+      draggable
+      pauseOnHover
+      theme="light"
+      transition={Bounce} /><>
+        {authroutes.includes(location.pathname) ? <L_R_Header /> : <Header />}
+        <Outlet />
+        {!authroutes.includes(location.pathname) && <Footer />}
+      </></>
   )
 }
 
