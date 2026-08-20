f459885fbd02b12966c62ca492b810e7f0e0db82
Farzan Ali <farzan.16.fa@gmail.com>
2026-08-20 22:42:36 +0500

    Add motion library

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -6,16 +6,39 @@ import { Navigate, useNavigate } from "react-router-dom";
 
 function Home() {
   const [isAdmin, setisAdmin] = useState(false);
-  const [user, setUser] = useState([]);
+  const [user, setUser] = useState({});
+  const [showform, setShowForm] = useState(false);
+  // const [showUser, setShowUser] = useState(false);
+  const [company, setCompany] = useState("");
+  const [role, setRole] = useState("");
+  const [status, setStatus] = useState("pending");
+
+  const storageKey = `applications_${user.userName}`;
+  const [application, setApplication] = useState(
+    () => [JSON.parse(localStorage.getItem("applications"))] || [],
+  );
 
   useEffect(() => {
     const admin = JSON.parse(localStorage.getItem("adminCred"));
+    // console.log(admin[0].isAdmin)
     setisAdmin(admin[0].isAdmin);
   }, []);
 
   const getUserData = () => {
-    setUser(JSON.parse(localStorage.getItem("User")) || []);
+    setUser(JSON.parse(localStorage.getItem("loggedIn_User")) || []);
   };
+
+  const handleClick = () => {
+    if (isAdmin) {
+      setShowUser(true);
+    } else {
+      setShowForm(true);
+    }
+  };
+
+  useEffect(() => {
+    getUserData();
+  }, []);
 
   const navigate = useNavigate();
 
@@ -47,9 +70,21 @@ function Home() {
     }
   }, [isAdmin]);
 
+  const handleApplication = (Usercompany, Userrole, Jobstatus) => {
+    const newApp = {
+      id: Date.now(),
+      company: Usercompany,
+      role: Userrole,
+      status: Jobstatus,
+    };
+
+    setApplication((prev) => [...prev, newApp]);
+  };
+
   useEffect(() => {
-    getUserData();
-  }, []);
+    localStorage.setItem(storageKey, JSON.stringify(application));
+  }, [application, storageKey]);
+
   return (
     <>
       <section className="w-full min-h-screen bg-[#F7F7F5] flex items-center justify-center px-6">
@@ -90,7 +125,7 @@ function Home() {
 
           <div className="flex gap-2">
             <button
-              onClick={() => navigate("/application")}
+              onClick={handleClick}
               className="bg-[#F5A623] text-[#1B3A6B] font-semibold px-8 py-3 rounded-full hover:bg-[#e09615] transition cursor-pointer"
             >
               {isAdmin ? "View All Users" : "+ Add Application"}
@@ -103,6 +138,35 @@ function Home() {
               {isAdmin ? "Manage Users" : "View Application"}
             </button>
           </div>
+          {showform && (
+            <div className="flex flex-col gap-3 p-2">
+              <input
+                type="text"
+                placeholder="Company"
+                value={company}
+                onChange={(e) => setCompany(e.target.value)}
+              />
+
+              <input
+                type="text"
+                placeholder="Role"
+                value={company}
+                onChange={(e) => setCompany(e.target.value)}
+              />
+
+              <select
+                value={status}
+                onChange={(e) => setStatus(e.target.value)}
+              >
+                <option value="pending">Pending</option>
+                <option value="accepted">Accepted</option>
+                <option value="rejected">Rejected</option>
+              </select>
+              <p>{user.userName}</p>
+              <button>save</button>
+              <button>cancel</button>
+            </div>
+          )}
         </div>
       </section>
     </>
