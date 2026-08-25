aaabcc5d27c862a48c3b9d122f50a46a5623f5a8
Farzan Ali <farzan.16.fa@gmail.com>
2026-08-25 22:41:51 +0500

    Change navlink color

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,53 +1,51 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
+import React from "react";
+import { NavLink } from "react-router-dom";
 function Footer() {
   return (
-    <div className='bg-[#1B3A6B] flex justify-between items-center gap-2 p-2 w-full'>
-       <div>
-           <h1 className="font-bold text-2xl text-[#F7F7F5]">careerTrack.</h1>
-          </div>
-      <div className='text-white flex justify-center items-center '>
-        <p>© 2026 CareerTrack. All rights reserved.</p>
+    <div className="bg-[#1B3A6B] flex justify-between items-center gap-2 px-20 py-4 w-full">
+      <div>
+        <h1 className="font-bold text-2xl text-[#F7F7F5]">careerTrack.</h1>
       </div>
 
-        <div>
-         <ul className='flex gap-3'>
-                      <li>
-                        <NavLink
-                          to={"home"}
-                          className={({ isActive }) =>
-                            `px-4 py-2 rounded-xl font-semibold ${isActive ? "bg-[#1B3A6B] text-black" : "text-white"}`
-                          }
-                        >
-                          Home
-                        </NavLink>
-                      </li>
-                      <li>
-                        <NavLink
-                          to={"/Application"}
-                          className={({ isActive }) =>
-                            `px-4 py-2 rounded-xl font-semibold ${isActive ? "bg-[#1B3A6B] text-black" : "text-white"}`
-                          }
-                        >
-                          My Applications
-                        </NavLink>
-                      </li>
-                      <li>
-                        <NavLink
-                          to={"/account"}
-                          className={({ isActive }) =>
-                            `px-4 py-2 rounded-xl font-semibold ${isActive ? "bg-[#1B3A6B] text-black" : "text-white"}`
-                          }
-                        >
-                          My account
-                        </NavLink>
-                      </li>
-                    </ul>
-        </div>
-
-          
+      <div>
+        <p className="text-white">© 2026 CareerTrack. All rights reserved.</p>
+      </div>
+      <div>
+        <ul className="flex flex-col gap-3">
+          <li>
+            <NavLink
+              to={"home"}
+              className={({ isActive }) =>
+                `px-4 py-2 rounded-xl font-semibold ${isActive ? "bg-[#1B3A6B] text-[#F5A623]" : "text-white"}`
+              }
+            >
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to={"/Application"}
+              className={({ isActive }) =>
+                `px-4 py-2 rounded-xl font-semibold ${isActive ? "bg-[#1B3A6B] text-[#F5A623]" : "text-white"}`
+              }
+            >
+              My Applications
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to={"/account"}
+              className={({ isActive }) =>
+                `px-4 py-2 rounded-xl font-semibold ${isActive ? "bg-[#1B3A6B] text-[#F5A623]" : "text-white"}`
+              }
+            >
+              My account
+            </NavLink>
+          </li>
+        </ul>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
