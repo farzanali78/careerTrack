3a00fca1b162b493bc48d2e4ff2a209392639d91
Farzan Ali <farzan.16.fa@gmail.com>
2026-08-26 22:58:21 +0500

    Add reponsiveness to footer component

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,51 +1,42 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 function Footer() {
-  return (
-    <div className="bg-[#1B3A6B] flex justify-between items-center gap-2 px-20 py-4 w-full">
-      <div>
+ return(
+   <div className="bg-[#1B3A6B] flex flex-col md:grid md:grid-cols-3 md:items-center gap-4 px-6 md:px-12 py-6 w-full">
+      
+      
+      <div className="text-center md:text-left">
         <h1 className="font-bold text-2xl text-[#F7F7F5]">careerTrack.</h1>
       </div>
 
-      <div>
-        <p className="text-white">© 2026 CareerTrack. All rights reserved.</p>
-      </div>
-      <div>
-        <ul className="flex flex-col gap-3">
+      
+      <div className="flex justify-center md:order-3 md:justify-end">
+        <ul className="flex flex-row gap-3">
           <li>
-            <NavLink
-              to={"home"}
-              className={({ isActive }) =>
-                `px-4 py-2 rounded-xl font-semibold ${isActive ? "bg-[#1B3A6B] text-[#F5A623]" : "text-white"}`
-              }
-            >
+            <NavLink to={"home"} className={({ isActive }) =>
+                    `px-4 py-2 rounded-xl font-semibold ${
+      isActive ? "bg-[#1B3A6B] text-[#F5A623]" : "text-white"}`
+                  }>
               Home
             </NavLink>
           </li>
           <li>
-            <NavLink
-              to={"/Application"}
-              className={({ isActive }) =>
-                `px-4 py-2 rounded-xl font-semibold ${isActive ? "bg-[#1B3A6B] text-[#F5A623]" : "text-white"}`
-              }
-            >
+            <NavLink to={"/Application"}  className={({ isActive }) =>
+                    `px-4 py-2 rounded-xl font-semibold ${
+      isActive ? "bg-[#1B3A6B] text-[#F5A623]" : "text-white"}`
+                  }>
               My Applications
-            </NavLink>
-          </li>
-          <li>
-            <NavLink
-              to={"/account"}
-              className={({ isActive }) =>
-                `px-4 py-2 rounded-xl font-semibold ${isActive ? "bg-[#1B3A6B] text-[#F5A623]" : "text-white"}`
-              }
-            >
-              My account
             </NavLink>
           </li>
         </ul>
       </div>
+
+    
+      <div className="text-center md:order-2">
+        <p className="text-white">© 2026 CareerTrack. All rights reserved.</p>
+      </div>
     </div>
-  );
+ )
 }
 
 export default Footer;
