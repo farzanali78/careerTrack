4106b2665655bfaf62dc192b0b267d211fc0d843
Farzan Ali <farzan.16.fa@gmail.com>
2026-08-26 23:01:11 +0500

    Add new layout

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Home from './components/Home/Home'
 import Login from './components/Login/Login'
 import Register from './components/Register/Register'
 import Application from './components/Application/Applications'
+import ShowUsers from "./components/Application/ShowUsers"
 import Account from "./components/Account/Account"
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
       <Route path="register" element={<Register/>}/>
       <Route path="application" element={<Application/>}/>
       <Route path="account" element={<Account/>}/>
+      <Route path="showUsers" element={<ShowUsers/>}/>
       </Route>
      
      </Routes>
