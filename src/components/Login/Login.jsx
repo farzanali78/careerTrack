5b06755f44c815b89b228bfc6d4c1ecf814c0c96
Farzan Ali <farzan.16.fa@gmail.com>
2026-08-27 23:40:50 +0500

    Add privacy icons and animations

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -7,6 +7,9 @@ import { Mail01Icon, LockPasswordIcon } from "@hugeicons/core-free-icons";
 import { useEffect } from "react";
 import { ToastContainer, toast, Bounce } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { motion } from "motion/react";
+import { Eye, EyeOff } from "lucide-react";
+import { AnimatePresence } from "motion/react";
 
 function Login() {
   const [userName, setUserName] = useState("");
@@ -33,9 +36,8 @@ function Login() {
   };
 
   const getUserData = () => {
-  setUser(JSON.parse(localStorage.getItem("User")) || []);
-};
-
+    setUser(JSON.parse(localStorage.getItem("User")) || []);
+  };
 
   useEffect(() => {
     setAdminData();
@@ -51,22 +53,20 @@ function Login() {
         setAdminData(true);
         setUserName("");
         setPassword("");
-          toast.success("Welcome Admin", {
-        position: "top-right",
-        autoClose: 5000,
-        hideProgressBar: false,
-        closeOnClick: false,
-        pauseOnHover: true,
-        draggable: true,
-        progress: undefined,
-        theme: "dark",
-        transition: Bounce,
-      });
-        navigate('home')
+        toast.success("Welcome Admin", {
+          position: "top-right",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: false,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "dark",
+          transition: Bounce,
+        });
+        navigate("home");
       }
     });
-
-    
 
     if (user.length === 0) {
       toast.error("User not found! create account first", {
@@ -89,19 +89,19 @@ function Login() {
         found = true;
         setUserName("");
         setPassword("");
-        localStorage.setItem('loggedIn_User',JSON.stringify(u))
-         toast.success(`Welcome ${u.userName}`, {
-        position: "top-right",
-        autoClose: 5000,
-        hideProgressBar: false,
-        closeOnClick: false,
-        pauseOnHover: true,
-        draggable: true,
-        progress: undefined,
-        theme: "dark",
-        transition: Bounce,
-      });
-        navigate('home')
+        localStorage.setItem("loggedIn_User", JSON.stringify(u));
+        toast.success(`Welcome ${u.userName}`, {
+          position: "top-right",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: false,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "dark",
+          transition: Bounce,
+        });
+        navigate("home");
       }
     });
     if (!found) {
@@ -117,85 +117,91 @@ function Login() {
         transition: Bounce,
       });
     }
-    
   };
   return (
     <>
-    
-      <div className="flex justify-center items-center w-full min-h-screen bg-[#F7F7F5] px-8">
-        <div className="flex items-center justify-center gap-8 max-w-6xl">
-          <div className="hidden md:block w-[55%]">
-            <img
-              src={loginImg}
-              alt="loginImg"
-              className="rounded-xl w-full h-full object-cover"
-            />
-          </div>
-          <div className="bg-[#1B3A6B] md:w-[45%] flex flex-col gap-2 w-full max-w-sm rounded-xl p-6 sm:p-10">
-            <div>
-              <h1 className="font-bold text-center sm:text-3xl text-2xl text-white">
-                careerTrack.
-              </h1>
+      <AnimatePresence>
+        <motion.div
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          exit={{ opacity: 0, x: -100 }}
+          transition={{ duration: 0.3, delay: 0.05, ease: "easeOut" }}
+          className="flex justify-center items-center w-full min-h-screen bg-[#F7F7F5] px-8"
+        >
+          <div className="flex items-center justify-center gap-8 max-w-6xl">
+            <div className="hidden md:block w-[55%]">
+              <img
+                src={loginImg}
+                alt="loginImg"
+                className="rounded-xl w-full h-full object-cover"
+              />
             </div>
-            <div className="mt-12 w-full">
-              <div className="border-b-2 border-white flex items-center gap-3">
-                <HugeiconsIcon
-                  icon={Mail01Icon}
-                  size={24}
-                  color="#ffffff"
-                  strokeWidth={1.5}
-                />
-                <input
-                  type="text"
-                  onChange={(e) => setUserName(e.target.value)}
-                  value={userName}
-                  placeholder="Username"
-                  className="w-full py-2 bg-transparent text-white outline-none placeholder:text-gray-300"
-                />
+            <div className="bg-[#1B3A6B] md:w-[45%] flex flex-col gap-2 w-full max-w-sm rounded-xl p-6 sm:p-10">
+              <div>
+                <h1 className="font-bold text-center sm:text-3xl text-2xl text-white">
+                  careerTrack.
+                </h1>
               </div>
-              <div className="border-b-2 border-white flex items-center gap-3">
-                <HugeiconsIcon
-                  icon={LockPasswordIcon}
-                  size={24}
-                  color="#ffffff"
-                  strokeWidth={1.5}
-                />
-                <input
-                  type={showPassword ? "text" : "password"}
-                  onChange={(e) => setPassword(e.target.value)}
-                  value={password}
-                  placeholder="Password"
-                  className="w-full py-2 bg-transparent text-white outline-none placeholder:text-gray-300"
-                />
-              </div>
-              <div className="flex mt-2 justify-end items-end">
-                <button
-                  onClick={() => setshowPassword(!showPassword)}
-                  className="underline border-none text-white cursor-pointer text-sm"
-                >
-                  Show Password
-                </button>
-              </div>
-              <div className="mt-4 flex items-center justify-center">
-                <button
-                  className="text-white border-2 cursor-pointer px-3 py-2 hover:bg-white hover:text-cyan-500 duration-75 ease-out border-cyan-500 rounded-xl font-semibold"
-                  onClick={() => handleUserData(userName, password)}
-                >
-                  Login
-                </button>
-              </div>
-              <div className="flex justify-center mt-2">
-                <p className="font-semibold text-sm text-white">
-                  Dont't have an account?{" "}
-                  <Link to="/register" className="hover:text-cyan-500">
-                    Register here
-                  </Link>
-                </p>
+              <div className="mt-12 w-full">
+                <div className="border-b-2 border-white flex items-center gap-3">
+                  <HugeiconsIcon
+                    icon={Mail01Icon}
+                    size={24}
+                    color="#ffffff"
+                    strokeWidth={1.5}
+                  />
+                  <input
+                    type="text"
+                    onChange={(e) => setUserName(e.target.value)}
+                    value={userName}
+                    placeholder="Username"
+                    className="w-full py-2 bg-transparent text-white outline-none placeholder:text-gray-300"
+                  />
+                </div>
+                <div className="border-b-2 border-white flex items-center gap-3">
+                  <HugeiconsIcon
+                    icon={LockPasswordIcon}
+                    size={24}
+                    color="#ffffff"
+                    strokeWidth={1.5}
+                  />
+                  <input
+                    type={showPassword ? "text" : "password"}
+                    onChange={(e) => setPassword(e.target.value)}
+                    value={password}
+                    placeholder="Password"
+                    className="w-full py-2 bg-transparent text-white outline-none placeholder:text-gray-300"
+                  />
+                  <button
+                    type="button"
+                    onClick={() => setshowPassword(!showPassword)}
+                    className="text-white"
+                  >
+                    {showPassword ? <EyeOff size={15} /> : <Eye size={15} />}
+                  </button>
+                </div>
+                
+                <div className="mt-4 flex items-center justify-center">
+                  <button
+                    className="text-white border-2 cursor-pointer px-3 py-2 hover:bg-white hover:text-cyan-500 duration-75 ease-out border-cyan-500 rounded-xl font-semibold"
+                    onClick={() => handleUserData(userName, password)}
+                  >
+                    Login
+                  </button>
+                </div>
+                <div className="flex justify-center mt-2">
+                  <p className="font-semibold text-sm text-white">
+                    Dont't have an account?{" "}
+                    <Link to="/register" className="hover:text-cyan-500">
+                      Register here
+                    </Link>
+                  </p>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-      </div>
+        </motion.div>
+      </AnimatePresence>
     </>
   );
 }
