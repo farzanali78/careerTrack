b953e72a99bee16e9c4dbad49bc19165b34415c1
Farzan Ali <farzan.16.fa@gmail.com>
2026-08-27 23:30:54 +0500

    Add privacy icons

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -5,6 +5,10 @@ import { useNavigate } from "react-router-dom";
 import { Link } from "react-router-dom";
 import { ToastContainer, toast, Bounce } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { motion } from "motion/react";
+import { AnimatePresence } from "motion/react";
+import { Eye, EyeOff } from "lucide-react";
+
 import {
   Mail01Icon,
   LockPasswordIcon,
@@ -22,7 +26,7 @@ function Register() {
   const [password, setPassword] = useState("");
   const [confirmPassword, setconfirmPassword] = useState("");
   const [confirmShowPassword, setconfirmShowPassword] = useState(false);
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   const [userCred, setUserCred] = useState(() => {
     return JSON.parse(localStorage.getItem("User")) || [];
   });
@@ -37,10 +41,6 @@ function Register() {
     confirmPass,
     contactNum,
   ) => {
-    
-   
-  
-
     if (!email || !userName || !userPass || !confirmPass || !contactNum) {
       toast.error("Fill all fields!", {
         position: "top-right",
@@ -87,23 +87,19 @@ function Register() {
       return;
     }
 
-    if (userCred.some((u) => u.userName === userName)){
-    toast.error("Username already exists");
-    setUserName("")
-    return
-   }
-   
-   else if (userCred.some((u) => u.userContactNum === contactNum)){
-   toast.error("Contact number already exists")
-    setContactNum("")
-    return
-   }
-   
-   else if (userCred.some((u) => u.Email === email)){
-    toast.error("Email already exists")
-    setEmail("")
-    return
-   }
+    if (userCred.some((u) => u.userName === userName)) {
+      toast.error("Username already exists");
+      setUserName("");
+      return;
+    } else if (userCred.some((u) => u.userContactNum === contactNum)) {
+      toast.error("Contact number already exists");
+      setContactNum("");
+      return;
+    } else if (userCred.some((u) => u.Email === email)) {
+      toast.error("Email already exists");
+      setEmail("");
+      return;
+    }
 
     const newUser = {
       id: Date.now(),
@@ -133,145 +129,153 @@ function Register() {
       transition: Bounce,
     });
 
-    
-    navigate("/")
+    navigate("/");
   };
   return (
     <>
-     
-      <div className="flex justify-center items-center w-full min-h-screen bg-[#F7F7F5] px-8">
-        <div className="flex items-center justify-center gap-8 max-w-6xl">
-          <div className="hidden md:block w-[55%]">
-            <img
-              src={registerImg}
-              alt="registerImg"
-              className="rounded-xl w-full h-full object-cover"
-            />
-          </div>
-
-          <div className="bg-[#1B3A6B] flex flex-col gap-2 w-full max-w-sm rounded-xl p-6 sm:p-10">
-            <div>
-              <h1 className="font-bold text-center sm:text-3xl text-2xl text-white">
-                careerTrack.
-              </h1>
+      <AnimatePresence>
+        <motion.div
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          exit={{ opacity: 0, x: -100 }}
+          transition={{ duration: 0.3, delay: 0.05, ease: "easeOut" }}
+          className="flex justify-center items-center w-full min-h-screen bg-[#F7F7F5] px-8"
+        >
+          <div className="flex items-center justify-center gap-8 max-w-6xl">
+            <div className="hidden md:block w-[55%]">
+              <img
+                src={registerImg}
+                alt="registerImg"
+                className="rounded-xl w-full h-full object-cover"
+              />
             </div>
-            <div className="mt-12 w-full">
-              <div className="border-b-2 border-white flex items-center gap-3">
-                <HugeiconsIcon
-                  icon={Mail01Icon}
-                  size={24}
-                  color="#ffffff"
-                  strokeWidth={1.5}
-                />
-                <input
-                  type="text"
-                  onChange={(e) => setEmail(e.target.value)}
-                  value={email}
-                  placeholder="Email"
-                  className="w-full py-2 bg-transparent text-white outline-none placeholder:text-gray-300"
-                />
-              </div>
-              <div className="border-b-2 border-white flex items-center gap-3">
-                <HugeiconsIcon
-                  icon={UserIcon}
-                  size={24}
-                  color="#ffffff"
-                  strokeWidth={1.5}
-                />
-                <input
-                  type="text"
-                  onChange={(e) => setUserName(e.target.value)}
-                  value={userName}
-                  placeholder="Username"
-                  className="w-full py-2 bg-transparent text-white outline-none placeholder:text-gray-300"
-                />
-              </div>
-              <div className="border-b-2 border-white flex items-center gap-3">
-                <HugeiconsIcon
-                  icon={Contact02Icon}
-                  size={24}
-                  color="#ffffff"
-                  strokeWidth={1.5}
-                />
-                <input
-                  type="text"
-                  onChange={(e) => setContactNum(e.target.value)}
-                  value={contactNum}
-                  placeholder="Contact"
-                  className="w-full py-2 bg-transparent text-white outline-none placeholder:text-gray-300"
-                />
-              </div>
-              <div className="border-b-2 border-white flex items-center gap-3">
-                <HugeiconsIcon
-                  icon={LockPasswordIcon}
-                  size={24}
-                  color="#ffffff"
-                  strokeWidth={1.5}
-                />
-                <input
-                  type={showPassword ? "text" : "password"}
-                  onChange={(e) => setPassword(e.target.value)}
-                  value={password}
-                  placeholder="Password"
-                  className="w-full py-2 bg-transparent text-white outline-none placeholder:text-gray-300"
-                />
-                <button
-                  className="text-white"
-                  onClick={() => setShowPassword(!showPassword)}
-                >
-                  Show
-                </button>
-              </div>
-              <div className="border-b-2 border-white flex items-center gap-3">
-                <HugeiconsIcon
-                  icon={LockPasswordIcon}
-                  size={24}
-                  color="#ffffff"
-                  strokeWidth={1.5}
-                />
-                <input
-                  type={confirmShowPassword ? "text" : "password"}
-                  onChange={(e) => setconfirmPassword(e.target.value)}
-                  value={confirmPassword}
-                  placeholder="Confirm Password"
-                  className="w-full py-2 bg-transparent text-white outline-none placeholder:text-gray-300"
-                />
-                <button
-                  className="text-white"
-                  onClick={() => setconfirmShowPassword(!confirmShowPassword)}
-                >
-                  Show
-                </button>
-              </div>
 
-              <div className="mt-4 flex items-center justify-center">
-                <button
-                  className="text-white border-2 cursor-pointer px-3 py-2 hover:bg-white hover:text-cyan-500 duration-75 ease-out border-cyan-500 rounded-xl font-semibold"
-                  onClick={() =>
-                    handleUserCred(
-                      email,
-                      userName,
-                      password,
-                      confirmPassword,
-                      contactNum,
-                    )
-                  }
-                >
-                  Register
-                </button>
+            <div className="bg-[#1B3A6B] flex flex-col gap-2 w-full max-w-sm rounded-xl p-6 sm:p-10">
+              <div>
+                <h1 className="font-bold text-center sm:text-3xl text-2xl text-white">
+                  careerTrack.
+                </h1>
               </div>
-              <div className="flex justify-center mt-2">
-                <p className="font-semibold text-sm text-white">
-                  Already have an account?{" "}
-                  <Link to="/" className="hover:text-cyan-500">
-                    Login here
-                  </Link>
-                </p>
+              <div className="mt-12 w-full">
+                <div className="border-b-2 border-white flex items-center gap-3">
+                  <HugeiconsIcon
+                    icon={Mail01Icon}
+                    size={24}
+                    color="#ffffff"
+                    strokeWidth={1.5}
+                  />
+                  <input
+                    type="text"
+                    onChange={(e) => setEmail(e.target.value)}
+                    value={email}
+                    placeholder="Email"
+                    className="w-full py-2 bg-transparent text-white outline-none placeholder:text-gray-300"
+                  />
+                </div>
+                <div className="border-b-2 border-white flex items-center gap-3">
+                  <HugeiconsIcon
+                    icon={UserIcon}
+                    size={24}
+                    color="#ffffff"
+                    strokeWidth={1.5}
+                  />
+                  <input
+                    type="text"
+                    onChange={(e) => setUserName(e.target.value)}
+                    value={userName}
+                    placeholder="Username"
+                    className="w-full py-2 bg-transparent text-white outline-none placeholder:text-gray-300"
+                  />
+                </div>
+                <div className="border-b-2 border-white flex items-center gap-3">
+                  <HugeiconsIcon
+                    icon={Contact02Icon}
+                    size={24}
+                    color="#ffffff"
+                    strokeWidth={1.5}
+                  />
+                  <input
+                    type="text"
+                    onChange={(e) => setContactNum(e.target.value)}
+                    value={contactNum}
+                    placeholder="Contact"
+                    className="w-full py-2 bg-transparent text-white outline-none placeholder:text-gray-300"
+                  />
+                </div>
+                <div className="border-b-2 border-white flex items-center gap-3">
+                  <HugeiconsIcon
+                    icon={LockPasswordIcon}
+                    size={24}
+                    color="#ffffff"
+                    strokeWidth={1.5}
+                  />
+                  <input
+                    type={showPassword ? "text" : "password"}
+                    onChange={(e) => setPassword(e.target.value)}
+                    value={password}
+                    placeholder="Password"
+                    className="w-full py-2 bg-transparent text-white outline-none placeholder:text-gray-300"
+                  />
+                  <button
+                    type="button"
+                    onClick={() => setShowPassword(!showPassword)}
+                    className="text-white"
+                  >
+                    {showPassword ? <EyeOff size={15} /> : <Eye size={15} />}
+                  </button>
+                </div>
+                <div className="border-b-2 border-white flex items-center gap-3">
+                  <HugeiconsIcon
+                    icon={LockPasswordIcon}
+                    size={24}
+                    color="#ffffff"
+                    strokeWidth={1.5}
+                  />
+                  <input
+                    type={confirmShowPassword ? "text" : "password"}
+                    onChange={(e) => setconfirmPassword(e.target.value)}
+                    value={confirmPassword}
+                    placeholder="Confirm Password"
+                    className="w-full py-2 bg-transparent text-white outline-none placeholder:text-gray-300"
+                  />
+                  <button
+                    type="button"
+                     onClick={() => setconfirmShowPassword(!confirmShowPassword)}
+                    className="text-white"
+                  >
+                    {confirmShowPassword ? <EyeOff size={15} /> : <Eye size={15} />}
+                  </button>
+                </div>
+
+                <div className="mt-4 flex items-center justify-center">
+                  <button
+                    className="text-white border-2 cursor-pointer px-3 py-2 hover:bg-white hover:text-cyan-500 duration-75 ease-out border-cyan-500 rounded-xl font-semibold"
+                    onClick={() =>
+                      handleUserCred(
+                        email,
+                        userName,
+                        password,
+                        confirmPassword,
+                        contactNum,
+                      )
+                    }
+                  >
+                    Register
+                  </button>
+                </div>
+                <div className="flex justify-center mt-2">
+                  <p className="font-semibold text-sm text-white">
+                    Already have an account?{" "}
+                    <Link to="/" className="hover:text-cyan-500">
+                      Login here
+                    </Link>
+                  </p>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-      </div>
+        </motion.div>
+      </AnimatePresence>
     </>
   );
 }
