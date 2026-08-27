c55481b41bd0f81f06bcd1f22f644b74646bc72d
Farzan Ali <farzan.16.fa@gmail.com>
2026-08-27 23:30:20 +0500

    Addd header animation

--- src/components/Login_Register_Header/L_R_Header.jsx
+++ src/components/Login_Register_Header/L_R_Header.jsx
@@ -1,15 +1,20 @@
 import React from 'react'
+import { motion } from 'framer-motion'
 
 function L_R_Header() {
   return (
-     <div className="bg-[#1B3A6B] p-2 w-full">
+     <motion.div 
+      initial={{ opacity: 0, y: -20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.4, ease: "easeOut" }}
+     className="bg-[#1B3A6B] p-2 w-full">
         <div className="w-full max-w-6xl flex items-center mx-auto relative px-4 py-2">
 
           <div>
            <h1 className="font-bold text-3xl text-[#F7F7F5]">careerTrack.</h1>
           </div>
           </div>
-          </div>
+          </motion.div>
   )
 }
 
