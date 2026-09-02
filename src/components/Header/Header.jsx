0665b89e253756f0f2baa941d704e1cbfa4bbd8c
Farzan Ali <farzan.16.fa@gmail.com>
2026-09-02 16:17:48 +0500

    Fix li tag

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -51,7 +51,7 @@ function Header() {
                     `px-4 py-2 rounded-xl font-semibold ${isActive ? "bg-[#1B3A6B] text-white" : "text-black"}`
                   }
                 >
-                  {admin[0]?.isAdmin ? "Manage Users" : "My Applications"}
+                  {admin[0]?.isAdmin ? "Manage Users" : "My Jobs"}
                 </NavLink>
               </li>
             </ul>
